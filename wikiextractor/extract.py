114a6a13d54c51d1a1c043859e175eb27ce1015c
eliasow <eliasowilson@gmail.com>
2025-04-02 17:02:43 -0700

    Moved IGNORECASE flag to comply with regex format changes
    Removed (?i) from inside regex pattern and passed re.I flag in second argument as expected in python 3.12 version of re package.

--- wikiextractor/extract.py
+++ wikiextractor/extract.py
@@ -374,8 +374,8 @@ ExtLinkBracketedRegex = re.compile(
     re.S | re.U | re.I)
 EXT_IMAGE_REGEX = re.compile(
     r"""^(http://|https://)([^][<>"\x00-\x20\x7F\s]+)
-    /([A-Za-z0-9_.,~%\-+&;#*?!=()@\x80-\xFF]+)\.((?i)gif|png|jpg|jpeg)$""",
-    re.X | re.S | re.U)
+    /([A-Za-z0-9_.,~%\-+&;#*?!=()@\x80-\xFF]+)\.(gif|png|jpg|jpeg)$""",
+    re.X | re.S | re.U | re.I)
 
 
 def replaceExternalLinks(text):
